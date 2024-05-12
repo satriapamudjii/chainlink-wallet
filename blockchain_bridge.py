2527110c2cb733c856b33402c2f97d7d534a06dc
satriapamudjii <163331188+satriapamudjii@users.noreply.github.com>
2024-05-12 11:16:38 +0200

    clean code

--- blockchain_bridge.py
+++ blockchain_bridge.py
@@ -15,11 +15,21 @@ class BlockchainBridge:
         self.public_key = PUBLIC_KEY
 
     def transfer_asset(self, from_chain, to_chain, asset, amount, receiver):
-        if not all([self.is_chain_supported(chain) for chain in [from_chain, to_chain]]):
-            print(f'Chain(s) not supported: {from_chain}, {to_chain}. Only supports: {self.supported_chains}')
-            return False
+        if self.validate_chains(from_chain, to_chain):
+            self.initiate_transfer(from_chain, to_chain, asset, amount, receiver)
+        else:
+            self.display_chain_error(from_chain, to_chain)
+
+    def validate_chains(self, from_chain, to_chain):
+        return all([self.is_chain_supported(chain) for chain in [from_chain, to_chain]])
+
+    def initiate_transfer(self, from_chain, to_chain, asset, amount, receiver):
         print(f'Transferring {amount} {asset} from {from_chain} to {to_chain}. Receiver: {receiver}')
         return True
+
+    def display_chain_error(self, from_chain, to_chain):
+        print(f'Chain(s) not supported: {from_chain}, {to_chain}. Only supports: {self.supported_chains}')
+        return False
 
     def is_chain_supported(self, chain):
         return chain in self.supported_chains
